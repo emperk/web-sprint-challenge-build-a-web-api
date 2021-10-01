e16b2688b5502013b729befd994c6e057ede4a83
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-10-01 13:19:21 -0700

    working

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,26 +1,41 @@
-// // add middlewares here related to projects
-// const Project = require('../projects/projects-model');
+// add middlewares here related to projects
+const Project = require('../projects/projects-model');
 
-// const validateProjectId = async (req, res, next) => {
-//   try {
-//     const project = await Project.get(req.params.id)
-//     if (!project) {
-//       res.status(404).json({
-//         message: ''
-//       })
-//     } else {
-//       req.project = project
-//       next()
-//     }
-//   } catch (err) {
-//     res.status(500).json({
-//       message: '',
-//       err: err.message,
-//       stack: err.stack
-//     })
-//   }
-// }
+const validateProjectId = async (req, res, next) => {
+  try {
+    const project = await Project.get(req.params.id)
+    if (!project) {
+      res.status(404).json({
+        message: ''
+      })
+    } else {
+      req.project = project
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: '',
+      err: err.message,
+      stack: err.stack
+    })
+  }
+}
 
-// module.exports = {
-//   validateProjectId,
-// }
+const validateProject = (req, res, next) => {
+  const { name, description, completed } = req.body
+  if (!name || !description || !completed) {
+    res.status(400).json({
+      message: ''
+    })
+  } else {
+    req.name = name
+    req.description = description
+    req.completed = completed
+    next()
+  }
+}
+
+module.exports = {
+  validateProjectId,
+  validateProject
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,6 @@
 // Write your "projects" router here!
 const express = require('express');
-// const { validateProjectId } = require('../projects/projects-middleware');
+const { validateProjectId, validateProject } = require('../projects/projects-middleware');
 const Project = require('./projects-model');
 
 const router = express.Router();
@@ -13,6 +13,41 @@ router.get('/', (req, res, next) => {
       res.json(projects)
     })
     .catch(next)
+})
+
+router.get('/:id', validateProjectId, (req, res, next) => {
+  res.json(req.project)
+})
+
+// .post()
+
+router.post('/', validateProject, (req, res, next) => {
+  Project.insert({ name: req.name, description: req.description, completed: req.completed })
+    .then(newProject => {
+      res.status(201).json(newProject)
+    })
+    .catch(next)
+})
+
+// .put()
+
+// router.put('/:id', validateProjectId, validateProject, (req, res, next) => {
+//   Project.update(req.params.id, { name: req.name, description: req.description, completed: req.completed })
+//     .then(updatedProject => {
+//       res.json(updatedProject)
+//     })
+//     .catch(next)
+// })
+
+// .delete()
+
+router.delete('/:id', validateProjectId, async (req, res, next) => {
+  try {
+    await Project.remove(req.params.id)
+    res.json(req.project)
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = router;
