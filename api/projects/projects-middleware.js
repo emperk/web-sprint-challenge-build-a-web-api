683fac80d75593ea9cdfe0b1207a9e9591a54b72
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-10-01 13:00:29 -0700

    working

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,26 @@
-// add middlewares here related to projects
+// // add middlewares here related to projects
+// const Project = require('../projects/projects-model');
+
+// const validateProjectId = async (req, res, next) => {
+//   try {
+//     const project = await Project.get(req.params.id)
+//     if (!project) {
+//       res.status(404).json({
+//         message: ''
+//       })
+//     } else {
+//       req.project = project
+//       next()
+//     }
+//   } catch (err) {
+//     res.status(500).json({
+//       message: '',
+//       err: err.message,
+//       stack: err.stack
+//     })
+//   }
+// }
+
+// module.exports = {
+//   validateProjectId,
+// }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,18 @@
 // Write your "projects" router here!
+const express = require('express');
+// const { validateProjectId } = require('../projects/projects-middleware');
+const Project = require('./projects-model');
+
+const router = express.Router();
+
+// .get()
+
+router.get('/', (req, res, next) => {
+  Project.get()
+    .then(projects => {
+      res.json(projects)
+    })
+    .catch(next)
+})
+
+module.exports = router;
